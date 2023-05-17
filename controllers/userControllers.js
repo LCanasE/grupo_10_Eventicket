8da132a1f74dfc948a6c1d6a753d7180b6361bec
LCanasE <lcanase@gmail.com>
2023-05-17 18:16:26 -0500

    Vistas renderizadas

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -4,10 +4,12 @@ const path = require('path');
 const userControllers = {
 
     getLogin: (req, res) =>
-    res.sendFile(path.join(__dirname, "../views/users/login.html")),
+    //res.sendFile(path.join(__dirname, "../views/users/login.html")),
+    res.render('./users/login.ejs'),
 
     getRegistro: (req, res) =>
-    res.sendFile(path.join(__dirname, "../views/users/registro.html"))
+    //res.sendFile(path.join(__dirname, "../views/users/registro.html"))
+    res.render('./users/registro.ejs')
 }
 
 module.exports = userControllers;
