79bcd929904fb6b56b24a074c97cd17e2bb554f8
LCanasE <lcanase@gmail.com>
2023-05-17 20:15:16 -0500

    Vistas parciales

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const userRoutes = require('./routers/userRoutes');
 
 app.set('view engine', 'ejs');
 app.set('views', [
+    path.join(__dirname, './views'),
     path.join(__dirname, './views/products'),
     path.join(__dirname, './views/users')
 ]), 
