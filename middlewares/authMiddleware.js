39798c4f9d9bb9c0d4a48b394aa494bb6f64a175
GBalatti9 <gas.balatti@gmail.com>
2023-07-10 20:03:33 -0300

    Detalle formulario

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,3 +1,4 @@
+// Si el usuario no está registrado, no se lo deja pasar y se lo redirige a al formulario de registro.
 const authMiddleware = (req, res, next) => {
     if(!req.session.user){
         return res.redirect('/users/register');
--- middlewares/categoryUserMiddleware.js
+++ middlewares/categoryUserMiddleware.js
@@ -1,7 +1,7 @@
 // Si el usuario registrado es de tipo "Espectador/a" no se lo deja ingresar a Crear Evento y se lo redirige a el formulario de edicion de usuario.
 const categoryUser = (req, res, next) => {
     if(req.session.user && req.session.user.tipoUsuario === "Espectador/a"){
-        return res.redirect('/users/editUser?=Queres cambiar de categoria');
+        return res.redirect('/users/editUser?=Si querés crear un evento, debés cambiar tu categoría de usuario');
     }
     
     next();
