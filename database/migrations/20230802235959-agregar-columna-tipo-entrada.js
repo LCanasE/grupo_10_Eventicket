369de0ce7e1d8d4bb88e9002b5850129d8f8e207
GBalatti9 <gas.balatti@gmail.com>
2023-08-02 21:03:30 -0300

    Migracion: se creo la columna ticket_type_id en la tabla carrito

---
+++ database/migrations/20230802235959-agregar-columna-tipo-entrada.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return await queryInterface.addColumn('user_product_cart', 'ticket_type_id', {
+      type: Sequelize.INTEGER 
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+       //   return await queryInterface.removeColumn('user_product_cart', 'ticket_type_id');
+  }
+};
