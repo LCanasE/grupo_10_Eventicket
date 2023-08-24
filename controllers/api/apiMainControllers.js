c8b9132dfb89a06c2a66853e910ab2d6580add6e
GBalatti9 <gas.balatti@gmail.com>
2023-08-24 18:45:57 -0300

    Api de productos y categorias

--- controllers/api/apiMainControllers.js
+++ controllers/api/apiMainControllers.js
@@ -1,16 +1,39 @@
-const { Product } = require('../../database/models');
+const { Product, Category } = require('../../database/models');
 
 module.exports = {
     getAll: async (req, res) => {
-        try {            
-            const allProducts = await Product.findAll();
+        const url = 'http://localhost:3000/api/products';
+
+        try {
+            const allCategories = await Category.findAll({
+                include: { association: "products" }
+            });
+            const filterAllCategories = allCategories.map(category => ({
+                name: category.name,
+                products: category.products.length,
+            }))
+            const allProducts = await Product.findAll({
+                include: { association: "categories" }
+            });
+            const filterAllProducts = allProducts.map(product => ({
+                id: product.id,
+                name: product.name,
+                date: product.date,
+                location: product.location,
+                addres: product.addres,
+                image: product.image,
+                category: product.categories,
+                detail: `${url}/${product.id}/eventDetails`
+            }))
             res.json(
                 {
-                    meta: {
-                        url: 'http://localhost:3000/api/products',
-                        total: allProducts.length
+                    url: url,
+                    count: allProducts.length,
+                    countByCategories: {
+                        categoriesCount: filterAllCategories.length,
+                        allCategories: filterAllCategories,
                     },
-                    results: allProducts
+                    results: filterAllProducts
                 })
         } catch (error) {
             console.log('API MAIN CONTROLLER', error);
