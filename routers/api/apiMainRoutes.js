5168433084b230676c144dd55f6efc5d6e633b05
GBalatti9 <gas.balatti@gmail.com>
2023-08-28 22:18:34 -0300

    Prueba

--- routers/api/apiMainRoutes.js
+++ routers/api/apiMainRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const apiMainControllers = require('../../controllers/api/apiMainControllers');
 const apiProductsControllers = require('../../controllers/api/apiProductsControllers');
+const apiUsersControllers = require('../../controllers/api/apiUsersControllers');
 
 const router = express.Router();
 
@@ -10,5 +11,8 @@ router.get('/products', apiMainControllers.getAll);
 
 // @GET /api/products/:id
 router.get('/products/:id', apiProductsControllers.getOne);
+
+// @GET /api/users
+router.get('/api/users', apiUsersControllers.getAllUsers);
 
 module.exports = router;
